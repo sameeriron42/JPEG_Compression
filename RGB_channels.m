e9e95cfae78180ecb7addda81a0f8a61c37970f3
sameerion42 <sameermark42@gmail.com>
2022-04-07 20:16:13 +0530

    slight improvements to RGB_channels.m

--- RGB_channels.m
+++ RGB_channels.m
@@ -1,27 +1,39 @@
-%RGB from image
+%Seperating RGB channels from an image
 
-img= imread("flower1.jpg");
+%read image file
+img= imread("images/flower1.jpg");
 
+%split rgb components using
+%R= img(:,:,1); red channel
+%G= img(:,:,2); green channel
+%B= img(:,:,3); blue channel
+%or
 [R,G,B]=imsplit(img);
+
 %plotting R,G,B channels alone shows only intensity for each spectrum as a
 %grayscale image, compose channel with reduntant black channel
 
 Black= zeros(size(img,1),size(img,2));
 
+% Create color versions of the individual color channels.
 Rimg=cat(3,R,Black,Black);
 Gimg=cat(3,Black,G,Black);
 Bimg=cat(3,Black,Black,B);
-%{
-Rimg=R;
-Gimg=G;
-Bimg=B;
-%}
+
+%display original image at centre and 3 rgb color images below it
+fontSize=20;
 subplot(2,3,2);
 imshow(img);
+title('RGB channels seperated','FontSize',fontSize);
+
 subplot(2,3,4);
 imshow(Rimg);
+title('Red Channel in Red', 'FontSize', fontSize)
+
 subplot(2,3,5);
 imshow(Gimg);
+title('Green Channel in Green', 'FontSize', fontSize)
+
 subplot(2,3,6);
 imshow(Bimg);
-title('RGB channels seperated');
+title('Blue Channel in Blue', 'FontSize', fontSize)
