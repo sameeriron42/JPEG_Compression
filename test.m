e127fe325d01fab93451b9ebc62140df47d12052
sameerion42 <sameermark42@gmail.com>
2022-04-16 16:33:14 +0530

    raw img folder added

--- test.m
+++ test.m
@@ -1,6 +1,33 @@
 close all;
 clc;
-a=imread('Screen.png');
+
+%a=imread('images/raw/');
+a=raw2rgb('images\raw\raw.NEF');
+%imshow(a);
+b=imcrop(a,[1 1,size(a,2) size(a,1)]);
+figure;
+temp=imfinfo('images\raw\_MG_7267.CR2').FileSize;
+size1=round(temp/1024^2,2);
+size2=round(imfinfo('result.jpg').FileSize/1024^2,2);
+string1 = {'the original size',strcat(string(size1),' MB')};
+string2 = {'the compressed size',strcat(string(size2),' MB')'};
+string3 = {'Compression factor',strcat(string(round(size1/size2)),'X')};
+subplot(1,2,1),imshow(a);
+title('Original Image','FontSize',16);
+annotation('textbox',[0.25 0.01 0.8 0.1],'String',string1,'FitBoxToText','on','FontSize',16);
+subplot(1,2,2),imshow(b);
+title('Compressed Image','FontSize',16);
+annotation('textbox',[0.68 0.01 0.8 0.1],'String',string2,'FitBoxToText','on','FontSize',16);
+annotation('textbox',[0.12 0.44 0.8 0.1],'String',string3,'FitBoxToText','on','FontSize',16,'HorizontalAlignment','center');
+
+%{
+a=2
+b=(zeros(1,16));
+for i=[1:16]
+    b(i)=a^i;
+end
+b
+%}
 r=255;
 c=r;
 %[r,c]=size(a)
@@ -13,5 +40,5 @@ for i= 1:r
     end
     k=0;
 end
-figure,imshow(img)
+%figure,imshow(img)
 
