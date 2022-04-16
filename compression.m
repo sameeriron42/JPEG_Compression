4a69144317fbcd71bb0705cf9c65b67954b63d63
sameerion42 <sameermark42@gmail.com>
2022-04-16 16:28:36 +0530

    clean up compression code

--- compression.m
+++ compression.m
@@ -2,14 +2,15 @@ clc;                %clear the command window
 %clear variables;    %clear all previously loaded variables
 close all;          %clopse all previously opened windows
 
-%I=imread("images\_MG_7267.CR2");
-
+fileName ='images/raw/raw.nef';
+%to import rgb images
+    %I=imread("images\food.jpg");
 %to import Color Filter array images(RAW imgs) use raw2rgb
-I=raw2rgb('images\RAW_NIKON_D1.NEF');
-figure,imshow(I);
+    I=raw2rgb(fileName);
+
 %Convert rgb image to YCbCr image
 YCbCr=rgb2ycbcr(I);
-figure,imshow(ycbcr2rgb(YCbCr));
+
 %Downsampling of blue and red chrominance
 %smoothen the image before downsampling
 filtr=ones(4,4)/16;
@@ -47,7 +48,6 @@ Qc= [17 18 24 47 99 99 99 99;
     99 99 99 99 99 99 99 99];
 Q=cat(3,Q,Qc,Qc);
 
-%{
 quality=input('Enter the quality setting(1<x<100): ');
 if quality > 50
     %decrease quantization table values by a factor of (100-q)/50
@@ -56,17 +56,18 @@ elseif quality < 50
     %increase quantization table values by 50/q
     Q=(50/quality)*Q;
 end
-%}
+
 blocksize=8;
 [row, col, dim]=size(YCbCr);
+%zero padding to make img multiple of 8
 row1=8*ceil(row/8);
 col1=8*ceil(col/8);
 YCbCr=padarray(YCbCr,[row1-row, col1-col],0,'post');
+
 dct_coeff=zeros([row col dim]);
 dct_quantized=dct_coeff;
 dct_dequant=dct_coeff;
 idct_signal=dct_coeff;
-
 for k = 1:3
     for i=1:blocksize:row
         for j=1:blocksize:col
@@ -84,32 +85,36 @@ end
 
 compressed_img=cat(3,idct_signal(:,:,1),idct_signal(:,:,2),idct_signal(:,:,3));
 clear dct_coeff dct_quantized dct_dequant idct_signal;
-figure,subplot(1,2,1);
-imshow(I);
-%F=rgb2gray(I);
-%surf(F(10:10:end,10:10:end));
-title('orginal');
-subplot(1,2,2);
 %JPEG files only support color space from 0-255
 %i.e, it does not support anything other than uint8
 if  not(isa(class(I),'uint8'))
-    disp('sis')
     compressed_img = cast(compressed_img,'uint16');
     compressed_img = im2uint8(compressed_img);
 end
 compressed_img=ycbcr2rgb(compressed_img);
-imshow(compressed_img);
-%F=rgb2gray(X);
-%surf(F(10:10:end,10:10:end));
-title('compress');
+%crop image [top,left bottom,right] as [(x,y) (x,y)]
+compressed_img=imcrop(compressed_img,[0 0 col row]);
 imwrite(compressed_img,'result.jpg');
 
+%text formating to show sizes and compression efficiency
+temp=imfinfo(fileName).FileSize;
+size1=round(temp/1024^2,2);
+size2=round(imfinfo('result.jpg').FileSize/1024^2,2);
+string1 = {'the original size',strcat(string(size1),' MB')};
+string2 = {'the compressed size',strcat(string(size2),' MB')'};
+string3 = {'Compression factor',strcat(string(round(size1/size2)),'X')};
 
+%Displaying output
+figure,subplot(1,2,1);
+imshow(I);
+title('Orginal Image','FontSize',16);
+subplot(1,2,2);
+imshow(compressed_img);
+title('Compressed Image','FontSize',16);
 
-
-
-
-
+annotation('textbox',[0.25 0.15 0.8 0.1],'String',string1,'FitBoxToText','on','FontSize',16);
+annotation('textbox',[0.68 0.15 0.8 0.1],'String',string2,'FitBoxToText','on','FontSize',16);
+annotation('textbox',[0.12 0.44 0.8 0.1],'String',string3,'FitBoxToText','on','FontSize',16,'HorizontalAlignment','center');
 
 
 
